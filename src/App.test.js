b89767610d16ad4e85ac39fa1afe534d67e747fe
KV Nguyen <kieuvyn12@gmail.com>
2019-08-27 22:59:36 -0400

    edited title of page on html, added minimal test for App.js rendering, installed testing frameworks

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import Enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+const adapter = new Adapter()
+Enzyme.configure({adapter})
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+describe('<App /> component', () => {
+  test('renders', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.exists()).toBe(true)
+  })
+})
