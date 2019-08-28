6fa0a0905dc08811dea5ef18a75002d9ea5a37d3
KV Nguyen <kieuvyn12@gmail.com>
2019-08-28 11:54:23 -0400

    added more specs for populating account nums and added descriptions to break up test specs

--- src/App.test.js
+++ src/App.test.js
@@ -22,86 +22,100 @@ describe('<App /> component', () => {
     const wrapper = shallow(<App />)
     expect(wrapper.exists()).to.equal(true)
   })
-  it('renders 9 userId buttons', () => {
-    const wrapper = shallow(<App />)
-    expect(wrapper.find('.userIdButton')).to.have.lengthOf(9)
+  describe('userId buttons', () => {
+    it('renders 9 userId buttons', () => {
+      const wrapper = shallow(<App />)
+      expect(wrapper.find('.userIdButton')).to.have.lengthOf(9)
+    })
   })
-  it('hits test API', async () => {
-    const wrapper = shallow(<App />)
-    const data = await wrapper.instance().getTransactions(3)
-    expect(data.length).to.equal(3)
+
+  describe('API response', () => {
+    it('hits test API', async () => {
+      const wrapper = shallow(<App />)
+      const data = await wrapper.instance().getTransactions(3)
+      expect(data.length).to.equal(3)
+    })
   })
-  it('sorts transactions in descending order', () => {
-    const wrapper = shallow(<App />)
-    const testTransactions = [{date: 10}, {date: 50}, {date: 1}]
-    const expectedSortedTestTransactions = [{date: 50}, {date: 10}, {date: 1}]
-    const data = wrapper.instance().sortByDateDesc(testTransactions)
-    expect(data[0].date).to.equal(expectedSortedTestTransactions[0].date)
-    expect(data[1].date).to.equal(expectedSortedTestTransactions[1].date)
-    expect(data[2].date).to.equal(expectedSortedTestTransactions[2].date)
+
+  describe('getting initial transactions from API and sorting', () => {
+    it('sorts transactions in descending order', () => {
+      const wrapper = shallow(<App />)
+      const testTransactions = [{date: 10}, {date: 50}, {date: 1}]
+      const expectedSortedTestTransactions = [{date: 50}, {date: 10}, {date: 1}]
+      const data = wrapper.instance().sortByDateDesc(testTransactions)
+      expect(data[0].date).to.equal(expectedSortedTestTransactions[0].date)
+      expect(data[1].date).to.equal(expectedSortedTestTransactions[1].date)
+      expect(data[2].date).to.equal(expectedSortedTestTransactions[2].date)
+    })
+    it('populates state with sorted transactions', async () => {
+      const wrapper = shallow(<App />)
+      const testTransactions = [{date: 10}, {date: 50}, {date: 1}]
+      const expectedSortedTestTransactions = [{date: 50}, {date: 10}, {date: 1}]
+      wrapper.instance().sortByDateDesc(testTransactions)
+      expect(wrapper.state().allTransactions[0].date).to.equal(
+        expectedSortedTestTransactions[0].date
+      )
+      expect(wrapper.state().allTransactions[1].date).to.equal(
+        expectedSortedTestTransactions[1].date
+      )
+      expect(wrapper.state().allTransactions[2].date).to.equal(
+        expectedSortedTestTransactions[2].date
+      )
+    })
   })
-  it('populates state with sorted transactions', async () => {
-    const wrapper = shallow(<App />)
-    const testTransactions = [{date: 10}, {date: 50}, {date: 1}]
-    const expectedSortedTestTransactions = [{date: 50}, {date: 10}, {date: 1}]
-    wrapper.instance().sortByDateDesc(testTransactions)
-    expect(wrapper.state().allTransactions[0].date).to.equal(
-      expectedSortedTestTransactions[0].date
-    )
-    expect(wrapper.state().allTransactions[1].date).to.equal(
-      expectedSortedTestTransactions[1].date
-    )
-    expect(wrapper.state().allTransactions[2].date).to.equal(
-      expectedSortedTestTransactions[2].date
-    )
-  })
-  it('populates state with account numbers', async () => {
-    const wrapper = shallow(<App />)
-    const testTransactions = [
-      {
-        id: 3,
-        category: 'supplies',
-        type: 'ACH In',
-        origin_account: 5432,
-        beneficiary_account: 2345,
-        amount: 879,
-        description: 'chairs',
-        initiator_id: 3,
-        approver_id: 2,
-        company_id: 1,
-        date: 1557187200000
-      },
-      {
-        id: 5,
-        category: 'maintenance',
-        type: 'Internal Transfer',
-        origin_account: 1234,
-        beneficiary_account: 2345,
-        amount: 145,
-        description: 'paper towels',
-        initiator_id: 3,
-        approver_id: 1,
-        company_id: 1,
-        date: 1552089600000
-      },
-      {
-        id: 25,
-        category: 'maintenance',
-        type: 'ACH In',
-        origin_account: 8542,
-        beneficiary_account: 2345,
-        amount: 134,
-        description: 'paper towels',
-        initiator_id: 3,
-        approver_id: 1,
-        company_id: 1,
-        date: 1554681600000
-      }
-    ]
-    wrapper.setState({allTransactions: testTransactions})
-    let accounts = wrapper.instance().initializeAccounts()
-    accounts = Array.from(accounts).sort()
-    expect(accounts[0]).to.equal(1234)
-    expect(accounts[1]).to.equal(2345)
+
+  describe('populating account numbers', () => {
+    it('populates state with account numbers', async () => {
+      const wrapper = shallow(<App />)
+      const testTransactions = [
+        {
+          id: 3,
+          category: 'supplies',
+          type: 'ACH In',
+          origin_account: 5432,
+          beneficiary_account: 2345,
+          amount: 879,
+          description: 'chairs',
+          initiator_id: 3,
+          approver_id: 2,
+          company_id: 1,
+          date: 1557187200000
+        },
+        {
+          id: 5,
+          category: 'maintenance',
+          type: 'Internal Transfer',
+          origin_account: 1234,
+          beneficiary_account: 2345,
+          amount: 145,
+          description: 'paper towels',
+          initiator_id: 3,
+          approver_id: 1,
+          company_id: 1,
+          date: 1552089600000
+        },
+        {
+          id: 25,
+          category: 'maintenance',
+          type: 'ACH In',
+          origin_account: 8542,
+          beneficiary_account: 2345,
+          amount: 134,
+          description: 'paper towels',
+          initiator_id: 3,
+          approver_id: 1,
+          company_id: 1,
+          date: 1554681600000
+        }
+      ]
+      wrapper.setState({allTransactions: testTransactions})
+      let accounts = wrapper.instance().initializeAccounts()
+      accounts = Array.from(accounts).sort()
+      expect(accounts[0]).to.equal(1234)
+      expect(accounts[1]).to.equal(2345)
+      let accountsStateArray = Array.from(wrapper.state().accounts).sort()
+      expect(accountsStateArray[0]).to.equal(1234)
+      expect(accountsStateArray[1]).to.equal(2345)
+    })
   })
 })
