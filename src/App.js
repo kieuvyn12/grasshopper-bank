a8a6d011c2d5b1b31b42cc3478c3cc6d67d92e08
KV Nguyen <kieuvyn12@gmail.com>
2019-08-27 22:59:02 -0400

    created buttons for selecting userIds which determine API transaction values which are being put onto state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userId: 0,
+      allTransactions: []
+    }
+    this.getUserId = this.getUserId.bind(this)
+    this.sortByDateDesc = this.sortByDateDesc.bind(this)
+  }
+
+  async getUserId(event) {
+    this.setState({
+      userId: event.target.value
+    })
+    let results = await axios.get(
+      `http://tech-challenge.d3ucrjz23k.us-east-1.elasticbeanstalk.com/transactions/${event.target.value}`
+    )
+    this.sortByDateDesc(results.data)
+  }
+
+  sortByDateDesc(transactions) {
+    this.setState({
+      allTransactions: transactions.sort((a, b) => b.date - a.date)
+    })
+  }
+
+  render() {
+    let userIds = new Array(9)
+    for (let i = 0; i < userIds.length - 1; i++) {
+      userIds[i] = i + 1
+    }
+    return (
+      <div className="App">
+        <div>
+          What is your user ID?
+          {userIds.map(userId => (
+            <button key={userId} onClick={this.getUserId} value={userId}>
+              {userId}
+            </button>
+          ))}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
